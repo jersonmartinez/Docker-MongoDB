c6b81ae261365919dff885ababe9c9e8cc791051
Antonio Moreno <jersonmartinezsm@gmail.com>
2022-06-24 12:22:29 -0600

    Adding docker-compose and JS script for credentials

---
+++ mongo-init.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: 'root-master',
+        pwd: 'password-master',
+        roles: [
+            {
+                role: 'readWrite',
+                db: 'db-master'
+            }
+        ]
+    }
+)
