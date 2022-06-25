ceaca3f02b0cbd0acfd261678f514d0946f0b92f
Antonio Moreno <jersonmartinezsm@gmail.com>
2022-06-25 06:03:10 -0600

    Adding new resources

--- app/import-mongo.py
+++ app/import-mongo.py
@@ -1,9 +1,8 @@
 import os
-import json
 import xmltodict
+import json
 import requests
 from pymongo import MongoClient
-
 
 class getScriptXML:
     def __init__(self, url, save_folder, filename):
@@ -40,66 +39,31 @@ class getScriptXML:
             jsonfileObj.write(jsonObj)
             jsonfileObj.close()
 
-        self.importJSONtoMongoDB()
+def importJSONtoMongoDB():
+    # Making Connection
+    mongoClient = MongoClient("mongodb://root-master:password-master@localhost:27017/?authMechanism=DEFAULT&authSource=db-master")
+    # mongoClient = MongoClient('localhost', 27017)
 
-    def importJSONtoMongoDB(self):
-        client = pymongo.MongoClient('mongodb://root-master:password-master@localhost:27017/?authMechanism=DEFAULT&authSource=db-master')
-        mydb = client['db-master']
-        mycol = mydb['sports']
+    # database
+    db = mongoClient["db-master"]
 
-        http_json = json.loads('JSON/sports.json')
-        # use iteritems() in Python 2 instead
-        for key, val in http_json.items():
+    # Created or Switched to collection
+    Collection = db["sports"]
 
-            # the value must be a list for insert_many() to work
-            if key == "docs" and type(val) == list:
-                mycol.insert_many(val)
+    # Loading or Opening the json file
+    with open('JSON/sports.json') as file:
+        file_data = json.load(file)
 
-        # declare an empty string object
-        # json_string = ''
-
-        # use Python's open() function to load a JSON file
-        # with open('JSON/sports.json', 'r', encoding='utf-8') as json_data:
-            # print('JSON/sports.json TYPE:', type(json_data))
-
-            # iterate over the _io.TextIOWrapper returned by open() using enumerate()
-            # for i, line in enumerate(json_data):
-                # append the parsed IO string to the JSON string
-                # json_string += line
-                
-        # print(json_string)
-        # Making Connection
-        # mongoClient = MongoClient("mongodb://root-master:password-master@localhost:27017/?authMechanism=DEFAULT")
-
-        # with open('JSON/sports.json') as f:
-        #     data = json.load(f)
-
-        
-
-        # mycol.insert_many(data, object)
-        # mycol.insert_many(json_string)
-
-        # for y in mycol.find():
-        #     print(y)
-
-        # database
-        # db = mongoClient["db-master"]
-
-        # Created or Switched to collection
-        # Collection = db["sports"]
-
-        # Loading or Opening the json file
-        # with open('JSON/sports.json') as file:
-            # file_data = json.load(file)
-
-        # print(file_data)
-
-        # Collection.insert_many(file_data, list)
-        # Collection.insert_one(file_data)
+    if isinstance(file_data, list):
+        Collection.insert_many(file_data)
+    else:
+        Collection.insert_one(file_data)
 
 if __name__ == "__main__":
-    getScriptXML(
-        'https://fx-nunchee-assets.s3.amazonaws.com/data/sports.xml',
-        'XML',
-        'sports.xml'
-    ).getData()
+    # getScriptXML(
+    #     'https://fx-nunchee-assets.s3.amazonaws.com/data/sports.xml',
+    #     'XML',
+    #     'sports.xml'
+    # ).getData()
+
+    importJSONtoMongoDB()
